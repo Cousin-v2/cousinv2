40641e9cfc632228f6235149314ed7639ce281df
Cousin <79681749+Cousin-v2@users.noreply.github.com>
2021-06-16 10:22:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 import asyncio
 import os
-
-os.system('pip install -U uvloop')
-
-import uvloop
 import sys
+import uvloop
 
-if not os.getenv('DEVICE_ID') and \
-        not os.getenv('ACCOUNT_ID') and \
-        not os.getenv('SECRET'):
-    print("Please paste your device auths into the \".env\" file.\n")
-    sys.exit()
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 os.system('pip install -U FNBOT2')
 os.system('clear')
 
 import FNBOT2
 
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-
 client = FNBOT2.PartyBot(
-    device_id=os.getenv('DEVICE_ID'),
-    account_id=os.getenv('ACCOUNT_ID'),
-    secret=os.getenv('SECRET')
+  device_id=os.getenv('DEVICE_ID'),
+  account_id=os.getenv('ACCOUNT_ID'),
+  secret=os.getenv('SECRET')
 )
 
 try:
